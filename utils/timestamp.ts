27dc14d71e830c44ea8c8b41bc2be30bf6ed5e8a
Logan McDaniel <lsmcd@pm.me>
2025-02-18 16:08:51 -0600

    Revert "Add additional tests"
    This reverts commit dc31a26d63d4f07f36b369c1a9d07ac6de6828a0.

--- utils/timestamp.ts
+++ utils/timestamp.ts
@@ -1,16 +1,10 @@
-const pattern =
-  /((1?[0-2]):([0-5]\d):([0-5]\d)) (PM|AM) (EST|(GMT|UTC)(\+|-)(1?\d))/g;
+const pattern = /(1?[0-2]:[0-5]\d:[0-5]\d) (PM|AM) (EST|UTC(\+|-)(1?\d))/g;
 
 function timestamp(timestamp: string): Date {
-  const groups = pattern.exec(timestamp);
-  if (groups) {
-    if (groups[6] === "EST") {
-      console.log();
-    }
+  if (timestamp.match(pattern)) {
     return new Date();
   }
   throw new Error("Invalid timestamp");
 }
-timestamp("12:00:00 AM EST");
 
 export default timestamp;
